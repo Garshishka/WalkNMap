3508f08056236ebf9308fe0230196618803a3ee6
garshishka <garshiv@gmail.com>
2023-06-27 23:27:43 +0300

    Added first version of algorithm to make points of a polygons

--- app/src/main/java/ru/garshishka/walknmap/Test.kt
+++ app/src/main/java/ru/garshishka/walknmap/Test.kt
@@ -3,6 +3,7 @@ package ru.garshishka.walknmap
 import android.graphics.Color
 import ru.garshishka.walknmap.data.MapPoint
 import ru.garshishka.walknmap.data.makePointMatrix
+import ru.garshishka.walknmap.data.makeWallsMatrix
 
 //TODO MOVE THEM BACK TO GLOBAL VARIABLES
 var SQUARE_COLOR = Color.argb(60, 43, 255, 251)
@@ -15,14 +16,84 @@ class Test {
             MapPoint(55.756, 37.754),
             MapPoint(55.757, 37.754),
             MapPoint(55.758, 37.754),
+            MapPoint(55.759, 37.754),
             MapPoint(55.756, 37.756),
-            MapPoint(55.758, 37.756),
+            MapPoint(55.759, 37.756),
+            MapPoint(55.756, 37.758),
+            MapPoint(55.759, 37.758),
+            MapPoint(55.756, 37.76),
+            MapPoint(55.757, 37.76),
+            MapPoint(55.758, 37.76),
+            MapPoint(55.759, 37.76),
         )
 
-        val pointMatrix = pointList.makePointMatrix()
+        val minPoint = MapPoint(pointList.minBy { it.lat }.lat, pointList.minBy { it.lon }.lon)
+        val maxPoint = MapPoint(pointList.maxBy { it.lat }.lat, pointList.maxBy { it.lon }.lon)
 
+        val rows = ((maxPoint.lat - minPoint.lat) / DOUBLE_LAT_ADJUSTMENT).toInt() + 1
+        val cols = ((maxPoint.lon - minPoint.lon) / DOUBLE_LON_ADJUSTMENT).toInt() + 1
+        println("$rows | $cols")
+
+        val pointMatrix = pointList.makePointMatrix(minPoint, rows, cols)
         for (row in pointMatrix) {
             println(row.contentToString())
         }
+
+
+        val wallMatrix = pointMatrix.makeWallsMatrix(rows, cols)
+        for (row in wallMatrix) {
+            println(row.contentToString())
+        }
+
+//val result = wallMatrix.makePolygonPointsLists(rows, cols)
+
+        val result = mutableListOf<MutableList<Pair<Int, Int>>>()
+
+        val indexMatrix = Array(rows + 2) { r ->
+            IntArray(cols + 2) { c -> -1 }
+        }
+        for (row in indexMatrix) {
+            println(row.contentToString())
+        }
+
+        for (r in 0..rows + 1) {
+            for (c in 0..cols + 1) {
+                if ((wallMatrix[r][c] and 1) == 1) {
+                    var i = r + 1
+                    var j = c
+                    val cycle = mutableListOf((i to j))
+                    indexMatrix[i][j] = 0
+                    while (true) {
+                        if (i < rows + 1 && (wallMatrix[i][j - 1] and 2) == 2) {
+                            wallMatrix[i][j - 1] -= 2
+                            i++
+                        } else if (i > 0 && (wallMatrix[i - 1][j - 1] and 2) == 2) {
+                            wallMatrix[i - 1][j - 1] -= 2
+                            i--
+                        } else if (j < cols + 1 && (wallMatrix[i - 1][j] and 1 == 1)) {
+                            wallMatrix[i - 1][j] -= 1
+                            j++
+                        } else if (j > 0 && (wallMatrix[i - 1][j - 1] and 1 == 1)) {
+                            wallMatrix[i - 1][j - 1] -= 1
+                            j--
+                        } else {
+                            break
+                        }
+                        cycle.add(i to j)
+                        val ix = indexMatrix[i][j]
+                        if (ix >= 0) {
+
+                        }
+                    }
+                    // result.add(cycle)
+                }
+            }
+        }
+
+        for (row in wallMatrix) {
+            println(row.contentToString())
+        }
+
+        result.forEach { println(it) }
     }
 }
--- app/src/main/java/ru/garshishka/walknmap/data/FogFunctions.kt
+++ app/src/main/java/ru/garshishka/walknmap/data/FogFunctions.kt
@@ -1,8 +1,6 @@
 package ru.garshishka.walknmap.data
 
 import com.yandex.mapkit.map.MapObjectCollection
-import ru.garshishka.walknmap.DOUBLE_LAT_ADJUSTMENT
-import ru.garshishka.walknmap.DOUBLE_LON_ADJUSTMENT
 import ru.garshishka.walknmap.LAT_ADJUSTMENT
 import ru.garshishka.walknmap.viewmodel.MainViewModel
 import kotlin.math.round
@@ -41,13 +39,7 @@ fun List<MapPoint>.addVerticalLinesOfFog(
     }
 }
 
-fun List<MapPoint>.makePointMatrix() : Array<IntArray> {
-    val minPoint = MapPoint(this.minBy { it.lat }.lat, this.minBy { it.lon }.lon)
-    val maxPoint = MapPoint(this.maxBy { it.lat }.lat, this.maxBy { it.lon }.lon)
-
-    val rows = ((maxPoint.lat - minPoint.lat) / DOUBLE_LAT_ADJUSTMENT).toInt() + 1
-    val cols = ((maxPoint.lon - minPoint.lon) / DOUBLE_LON_ADJUSTMENT).toInt() + 1
-
+fun List<MapPoint>.makePointMatrix(minPoint: MapPoint,rows: Int, cols: Int): Array<IntArray> {
     return Array(rows + 2) { r ->
         IntArray(cols + 2) { c ->
             if (r == 0 || c == 0 || r == rows + 1 || c == cols + 1) {
@@ -67,4 +59,51 @@ fun List<MapPoint>.makePointMatrix() : Array<IntArray> {
             }
         }
     }
+}
+
+fun Array<IntArray>.makeWallsMatrix(rows: Int, cols: Int): Array<IntArray> =
+    Array(rows + 2) { r ->
+        IntArray(cols + 2) { c ->
+            if(r==rows+1 || c == cols+1){
+                0
+            }else {
+                if (this[r][c] != this[r][c + 1]) {2} else {0} +
+                        if (this[r][c] != this[r + 1][c]) {1} else {0}
+            }
+        }
+    }
+
+fun Array<IntArray>.makePolygonPointsLists(rows: Int, cols: Int): MutableList<MutableList<Pair<Int,Int>>>{
+    val result = mutableListOf<MutableList<Pair<Int, Int>>>()
+
+    for (r in 0..rows+1) {
+        for (c in 0..cols+1) {
+            if ((this[r][c] and 1) == 1) {
+                var i = r + 1
+                var j = c
+                val cycle = mutableListOf((i to j))
+                while (true) {
+                    if (i < rows+1 && (this[i][j - 1] and 2) == 2) {
+                        this[i][j - 1] -= 2
+                        i++
+                    } else if (i > 0 && (this[i - 1][j - 1] and 2) == 2) {
+                        this[i - 1][j - 1] -= 2
+                        i--
+                    } else if (j < cols+1 && (this[i - 1][j] and 1 == 1)) {
+                        this[i - 1][j] -= 1
+                        j++
+                    } else if (j > 0 && (this[i - 1][j-1] and 1 == 1)) {
+                        this[i - 1][j-1] -= 1
+                        j--
+                    } else{
+                        break
+                    }
+                    cycle.add(i to j)
+                }
+                result.add(cycle)
+            }
+        }
+    }
+
+    return result
 }
